35e21786790be61ef358e0bedc0f6e5cbbb85342
Mattias Thalén <bitter-polders0x@icloud.com>
2025-01-30 15:04:33 +0100

    Explode CTE with union all by name

--- models/gold/uss__bridge.sql
+++ models/gold/uss__bridge.sql
@@ -3,53 +3,46 @@ MODEL (
   kind VIEW
 );
 
-WITH bridge AS (
-    SELECT
-    *
-    FROM gold.uss__bridge__categories
-    UNION ALL BY NAME
-    SELECT
-    *
-    FROM gold.uss__bridge__customers
-    UNION ALL BY NAME
-    SELECT
-    *
-    FROM gold.uss__bridge__employees
-    UNION ALL BY NAME
-    SELECT
-    *
-    FROM gold.uss__bridge__employee_territories
-    UNION ALL BY NAME
-    SELECT
-    *
-    FROM gold.uss__bridge__order_details
-    UNION ALL BY NAME
-    SELECT
-    *
-    FROM gold.uss__bridge__orders
-    UNION ALL BY NAME
-    SELECT
-    *
-    FROM gold.uss__bridge__products
-    UNION ALL BY NAME
-    SELECT
-    *
-    FROM gold.uss__bridge__regions
-    UNION ALL BY NAME
-    SELECT
-    *
-    FROM gold.uss__bridge__shippers
-    UNION ALL BY NAME
-    SELECT
-    *
-    FROM gold.uss__bridge__suppliers
-    UNION ALL BY NAME
-    SELECT
-    *
-    FROM gold.uss__bridge__territories
-)
-
 SELECT
-    COLUMNS(c -> c NOT LIKE '_sqlmesh_%'),
-    COLUMNS(c -> c LIKE '_sqlmesh_%')
-FROM bridge
+*
+FROM gold.uss__bridge__categories
+UNION ALL BY NAME
+SELECT
+*
+FROM gold.uss__bridge__customers
+UNION ALL BY NAME
+SELECT
+*
+FROM gold.uss__bridge__employees
+UNION ALL BY NAME
+SELECT
+*
+FROM gold.uss__bridge__employee_territories
+UNION ALL BY NAME
+SELECT
+*
+FROM gold.uss__bridge__order_details
+UNION ALL BY NAME
+SELECT
+*
+FROM gold.uss__bridge__orders
+UNION ALL BY NAME
+SELECT
+*
+FROM gold.uss__bridge__products
+UNION ALL BY NAME
+SELECT
+*
+FROM gold.uss__bridge__regions
+UNION ALL BY NAME
+SELECT
+*
+FROM gold.uss__bridge__shippers
+UNION ALL BY NAME
+SELECT
+*
+FROM gold.uss__bridge__suppliers
+UNION ALL BY NAME
+SELECT
+*
+FROM gold.uss__bridge__territories
