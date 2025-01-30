6d3089948051dbc1d45c4437d0e61a6faa91a3a4
Mattias Thalén <bitter-polders0x@icloud.com>
2025-01-30 22:25:51 +0100

    Reorder columns in int__measures__orders

--- models/silver/int__measures__orders.sql
+++ models/silver/int__measures__orders.sql
@@ -6,24 +6,24 @@ MODEL (
 WITH orders_placed AS (
   SELECT
     _hook__order__valid_from,
-    1 AS measure__orders_placed,
-    order_date AS _key__date
+    order_date AS _key__date,
+    1 AS measure__orders_placed
   FROM silver.bag__northwind__orders
   WHERE
     NOT bag__northwind__orders.order_date IS NULL
 ), orders_required AS (
   SELECT
     _hook__order__valid_from,
-    1 AS measure__orders_required,
-    required_date AS _key__date
+    required_date AS _key__date,
+    1 AS measure__orders_required
   FROM silver.bag__northwind__orders
   WHERE
     NOT bag__northwind__orders.required_date IS NULL
 ), orders_shipped AS (
   SELECT
     _hook__order__valid_from,
-    1 AS measure__orders_shipped,
-    shipped_date AS _key__date
+    shipped_date AS _key__date,
+    1 AS measure__orders_shipped
   FROM silver.bag__northwind__orders
   WHERE
     NOT bag__northwind__orders.shipped_date IS NULL
@@ -34,10 +34,10 @@ SELECT
     orders_shipped._hook__order__valid_from,
     orders_shipped._hook__order__valid_from
   ) AS _hook__order__valid_from,
+  COALESCE(orders_required._key__date, orders_shipped._key__date, orders_shipped._key__date) AS _key__date,
   orders_placed.measure__orders_placed,
   orders_required.measure__orders_required,
-  orders_shipped.measure__orders_shipped,
-  COALESCE(orders_required._key__date, orders_shipped._key__date, orders_shipped._key__date) AS _key__date
+  orders_shipped.measure__orders_shipped
 FROM orders_placed
 FULL OUTER JOIN orders_required
   ON orders_placed._hook__order__valid_from = orders_required._hook__order__valid_from
